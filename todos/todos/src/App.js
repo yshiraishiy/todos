9cef4bb1c347409d7ba108da71546a534ddc1dec
yshiraishiy <yuya0505soccer@gmail.com>
2022-04-04 21:48:44 +0900

    commit

--- todos/todos/src/App.js
+++ todos/todos/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import InputForm from './InputForm';
+import EditForm from './EditForm';
 
 const App = () => {
   
---
+++ todos/todos/src/EditForm.js
@@ -0,0 +1,14 @@
+const EditForm = () => {
+  <>
+    <input 
+    type='text'
+    label='新しいタイトル'
+    value={newTitle}
+    onChange={handleEditFormChanges}
+    />
+    <button onClick={() => handleEditTodo}>編集を保存</button>
+    <button onClick={() => handleCloseEditForm}>キャンセル</button>
+  </>
+}
+
+export default EditForm;
